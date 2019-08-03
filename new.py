914c8e114ecbf82b9f7942c7b020b74cfe1436b5
ssyadav2 <43070314+ssyadav2@users.noreply.github.com>
2019-08-03 17:42:14 -0500

    Update new.py

--- new.py
+++ new.py
@@ -127,7 +127,7 @@ def PCS(homecount, prim_amount, autocount, vehcount, max_age):
 
 # In[73]:
 
-time.sleep(240)
+time.sleep(120)
 connection = tabpy_client.Client('http://tp-server-cfd.herokuapp.com/')
 connection.deploy('PCS',PCS,
                   'Predicting probability of cross-sell from given data', override=True)
