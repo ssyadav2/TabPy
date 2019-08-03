2ba27a808e7d4b0e17a5516538a1b687c878cac4
ssyadav2 <43070314+ssyadav2@users.noreply.github.com>
2019-08-03 13:00:12 -0500

    Update SettingsParameters.py

--- tabpy-server/tabpy_server/app/SettingsParameters.py
+++ tabpy-server/tabpy_server/app/SettingsParameters.py
@@ -3,7 +3,7 @@ class SettingsParameters:
     Application (TabPyApp) settings names
     '''
     TransferProtocol = 'transfer_protocol'
-    Port = 443
+    Port = '443'
     ServerVersion = 'server_version'
     UploadDir = 'upload_dir'
     CertificateFile = 'certificate_file'
