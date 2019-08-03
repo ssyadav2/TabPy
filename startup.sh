7241eab68a1bda1f97961c5c084f5980709319fa
ssyadav2 <43070314+ssyadav2@users.noreply.github.com>
2019-08-03 18:00:58 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -106,6 +106,9 @@ export PYTHONPATH="${TABPY_ROOT}/tabpy-server:${TABPY_ROOT}/tabpy-tools:$PYTHONP
 if [ -z $CONFIG ]; then
     echo Using default parameters.
     python3 tabpy-server/tabpy_server/tabpy.py
+    bg
+    sleep 1m
+    python3 ${TABPY_ROOT}/models/setup.py
 else
     python3 tabpy-server/tabpy_server/tabpy.py --config=$CONFIG
 fi
